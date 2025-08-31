bcc5d07dbbaa82a6b5e822711b9ab6cde731fdad
Oswaldo Lopez <77045040+RomperS@users.noreply.github.com>
2025-08-31 07:16:01 +0000

    feat: Enable config server and discovery client capabilities

--- src/main/java/com/olo/configservice/ConfigServiceApplication.java
+++ src/main/java/com/olo/configservice/ConfigServiceApplication.java
@@ -3,8 +3,12 @@ package com.olo.configservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableConfigServer
 @EnableCaching
 public class ConfigServiceApplication {
 
