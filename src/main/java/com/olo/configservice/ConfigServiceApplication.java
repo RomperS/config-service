2037eccacf645ffc96cc17f1dc2e92c5574b3fc3
Oswaldo Lopez <77045040+RomperS@users.noreply.github.com>
2025-08-31 02:22:01 +0000

    feat(cache): add Caffeine and enable caching

--- src/main/java/com/olo/configservice/ConfigServiceApplication.java
+++ src/main/java/com/olo/configservice/ConfigServiceApplication.java
@@ -2,8 +2,10 @@ package com.olo.configservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class ConfigServiceApplication {
 
     public static void main(String[] args) {
